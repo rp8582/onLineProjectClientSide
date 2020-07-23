fc9c7709a155461cbe86d5cac859ce090d00dcfc
Sara <sk0533121840@gmail.com>
2020-07-23 17:12:56 +0300

    task completed

--- src/app/make-appointment/make-appointment.page.ts
+++ src/app/make-appointment/make-appointment.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
 import { BookAppointment } from '../bookAppointment.service';
+
 @Component({
   selector: 'app-make-appointment',
   templateUrl: './make-appointment.page.html',
@@ -27,29 +28,36 @@ export class MakeAppointmentPage implements OnInit {
   loadDays() {
     this.appointmentService.loadDays(this.selectedService.ServiceId).subscribe((days => {
       this.days = days
+      console.log(days);
     }));
   }
-  loadHours(day) {
-    this.selectedDay = day;
-    this.appointmentService.loadHoursPerDay(this.selectedService.ServiceId, day).subscribe((hours => {
-      console.log(hours);
-      this.hours = hours;
-    }))
-  }
+ 
   setHour(hour) {
     this.selectedHour = hour;
   }
 
   confirmTurn() {
-    //var date: Date;
-   // date = new Date();
+    var date: Date;
+    date = new Date(this.selectedDay+" "+this.selectedHour);
+    console.log("date",date);
+   
     //date.setDate(date.getDate() + (this.selectedDay - date.getDate()));
     
     this.appointmentService.makeAppointment
-    ({ PreAlert: this.preAlert, ServiceId: this.selectedService.ServiceId, EstimatedHour:this.selectedHour}).subscribe(
+    ({ PreAlert: this.preAlert, ServiceId: this.selectedService.ServiceId, EstimatedHour:this.selectedDay+" "+this.selectedHour}).subscribe(
       (state=>console.log(state))
     )
-
-   
   }
+  segmentChanged(ev: any) {
+    //console.log('Segment1', ev.detial.value);
+    var day= ev.detail.value;
+    console.log('Segment2',day);
+    this.selectedDay = day.split("T")[0];
+    this.appointmentService.loadHoursPerDay(this.selectedService.ServiceId, this.selectedDay).subscribe((hours => {
+      console.log(hours);
+      this.hours = hours;
+    }))
+    
+  }
+  
 }
